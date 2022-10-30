29e5df983efe80a098415863ab0002a27a7572c0
Dao <114120274+dqiu0@users.noreply.github.com>
2022-10-29 22:23:52 -0400

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,46 @@
+var words = ['Hi there!', 'This is my intro-->', ' I love programming @#$%^&*', 'I love playing games !w!', 'Discover cool things?','Here you can find my projects ...', 'Let me know if you like them!', 'Thank you for watching :)'],
+    part,
+    i = 0,
+    offset = 0,
+    len = words.length,
+    forwards = true,
+    skip_count = 0,
+    skip_delay = 15,
+    speed = 70;
+var wordflick = function () {
+  setInterval(function () {
+    if (forwards) {
+      if (offset >= words[i].length) {
+        ++skip_count;
+        if (skip_count == skip_delay) {
+          forwards = false;
+          skip_count = 0;
+        }
+      }
+    }
+    else {
+      if (offset == 0) {
+        forwards = true;
+        i++;
+        offset = 0;
+        if (i >= len) {
+          i = 0;
+        }
+      }
+    }
+    part = words[i].substring(0, offset);
+    if (skip_count == 0) {
+      if (forwards) {
+        offset++;
+      }
+      else {
+        offset--;
+      }
+    }
+    $('.word').text(part);
+  },speed);
+};
+
+$(document).ready(function () {
+  wordflick();
+});
